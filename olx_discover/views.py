4c0b7af7662d15a4d2dc24bd0f5aa5aa1f69701f
Marcos Felipe A. Ferreira <marcos.ferreira@neoprospecta.com>
2018-04-12 14:10:52 -0300

    update

--- olx_discover/views.py
+++ olx_discover/views.py
@@ -6,7 +6,7 @@ from olx_discover.utils import OlxDiscover
 
 @app.route('/')
 def hello_world():
-    olx = OlxDiscover('http://sc.olx.com.br/florianopolis-e-regiao/norte/imoveis/aluguel?')
+    olx = OlxDiscover('http://sc.olx.com.br/florianopolis-e-regiao/norte/imoveis/aluguel?sd=2557&sd=2555&sd=2546')
     ads = olx.get_only_valid_ads()
     # ads = Ad.query.all()
     return render_template('email.html', ad_list=ads)
